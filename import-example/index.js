4d19897f97141b401ede541f8396f809949faef0
gonzaemon <goki14914@icloud.com>
2020-05-11 11:53:02 +0900

    import-example/inde x.js

---
+++ import-example/index.js
@@ -0,0 +1,6 @@
+import { hello, goodbye } from './module'; // 複数のimportする場合
+// import * as xyz from './module'     全てをimportする場合
+// import xyz from './module'          defaultをimportする場合
+
+console.log(hello('wakuwaku bank'))
+console.log(goodbye('wakuwaku bank'))
